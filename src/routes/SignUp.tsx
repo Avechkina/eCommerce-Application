040d24f4c466b486ed3b73f9b7534e1ea49f3944
Timofey <naryshkint4@gmail.com>
2025-05-08 14:10:02 +0400

    feat: add link on the registration page that allows users to navigate to the login page

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,7 +1,15 @@
 import RegistrationForm from '@features/RegistrationForm/RegistrationForm';
+import { Link } from 'react-router';
 
 const SignUp = () => {
-  return <RegistrationForm />;
+  return (
+    <>
+      <p>
+        Already have an account? <Link to="/signin">Sign in</Link>
+      </p>
+      <RegistrationForm />
+    </>
+  );
 };
 
 export default SignUp;
