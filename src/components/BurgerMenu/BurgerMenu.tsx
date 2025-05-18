f67d4c35412fb8c6c354d4835dec0ff658688cd2
Наташка Овечкина <natallia.ovechkina@gmail.com>
2025-05-18 19:57:06 +0300

    feat: implement closing burger after click on link or outof burger menu

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './BurgerMenu.module.css';
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
@@ -6,22 +6,47 @@ import { NavLink } from 'react-router';
 
 const BurgerMenu: React.FC = () => {
   const [collapsed, setCollapsed] = useState(true);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
 
+  const handleLinkClick = () => {
+    setCollapsed(true);
+  };
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setCollapsed(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={classes.burger_icon}>
+    <div ref={menuRef} className={classes.burger_icon}>
       <Button type="primary" onClick={toggleCollapsed}>
         {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
       </Button>
       {!collapsed && (
         <div className={classes.burger}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/shop">Shop</NavLink>
-          <NavLink to="/product">Product</NavLink>
-          <NavLink to="/about">About us</NavLink>
+          <NavLink to="/" onClick={handleLinkClick}>
+            Home
+          </NavLink>
+          <NavLink to="/shop" onClick={handleLinkClick}>
+            Shop
+          </NavLink>
+          <NavLink to="/product" onClick={handleLinkClick}>
+            Product
+          </NavLink>
+          <NavLink to="/about" onClick={handleLinkClick}>
+            About us
+          </NavLink>
         </div>
       )}
     </div>
