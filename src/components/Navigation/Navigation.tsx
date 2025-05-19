45d7613a12a5fe015ebc2f1fcc7f9b3d460a7a90
Timofey <naryshkint4@gmail.com>
2025-05-19 20:30:24 +0400

    Revert "Merge branch 'release/login-registration-main' of https://github.com/Avechkina/eCommerce-Application into release/login-registration-main"
    This reverts commit ea070247a25de405a6a2b713804aa8bc6431bb41, reversing
    changes made to ed05fd9a9df00d86ed92f2dfb3cf64bdbb209b65.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,7 @@
 import { NavLink, useNavigate } from 'react-router';
 import classes from './Navigation.module.css';
-import { Button, Tooltip } from 'antd';
+import { Button } from 'antd';
 import useUserStore from '@store/userStore';
-import {
-  LoginOutlined,
-  LogoutOutlined,
-  UserAddOutlined,
-} from '@ant-design/icons';
-import BurgerMenu from '@components/BurgerMenu/BurgerMenu';
 
 const Navigation = () => {
   const navigate = useNavigate();
@@ -18,43 +12,22 @@ const Navigation = () => {
   };
   return (
     <nav className={classes.nav}>
-      <BurgerMenu />
-      <h2 className={classes.logo} onClick={() => navigate('/')}>FutureTech.</h2>
-      <div className={classes.link_wrapper}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/shop">Shop</NavLink>
-        <NavLink to="/product">Product</NavLink>
-        <NavLink to="/about">About us</NavLink>
-      </div>
-      <div className={classes.icon_wrapper}>
-        {isAuth ? (
-          <Tooltip title="Sign out">
-            <Button
-              icon={<LogoutOutlined />}
-              onClick={handleSignoutButtonClick}
-              type="link"
-            ></Button>
-          </Tooltip>
-        ) : (
-          <>
-            {' '}
-            <Tooltip title="Sign in">
-              <Button
-                icon={<LoginOutlined />}
-                onClick={() => navigate('/signin')}
-                type="link"
-              ></Button>
-            </Tooltip>
-            <Tooltip title="Sign up">
-              <Button
-                icon={<UserAddOutlined />}
-                onClick={() => navigate('/signup')}
-                type="link"
-              ></Button>
-            </Tooltip>
-          </>
-        )}
-      </div>
+      <NavLink to="/">Home</NavLink>
+      {isAuth ? (
+        <Button onClick={handleSignoutButtonClick} type="link">
+          Sign out
+        </Button>
+      ) : (
+        <>
+          {' '}
+          <Button onClick={() => navigate('/signin')} type="link">
+            Sign in
+          </Button>
+          <Button onClick={() => navigate('/signup')} type="primary">
+            Sign up
+          </Button>
+        </>
+      )}
     </nav>
   );
 };
