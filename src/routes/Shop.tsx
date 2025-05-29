abe074fc12af6a9db5cf0d041e7d196ad59e614c
Наташка Овечкина <natallia.ovechkina@gmail.com>
2025-05-29 13:48:08 +0300

    fix: correct problem whith shop page after merge conflict

--- src/routes/Shop.tsx
+++ src/routes/Shop.tsx
@@ -1,11 +1,11 @@
 import { Products } from '@components/Products/Products';
 
-const Shop = () => 
+const Shop = () => {
   return (
     <>
       <h1>Shop page</h1>
       <Products />
     </>
- );
-
+ )
+};
 export default Shop;
