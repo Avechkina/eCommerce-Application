7abc02eaf4185e9c47284913e401a38afcd38378
Vasilii Malai <malaivasili@gmail.com>
2025-06-03 02:22:38 +0300

    feat: add SearchProducts component to Layout

--- src/features/Catalog/CatalogLayout/CatalogLayout.tsx
+++ src/features/Catalog/CatalogLayout/CatalogLayout.tsx
@@ -2,13 +2,18 @@ import CatalogSidebar from '@features/Catalog/CatalogSidebar/CatalogSidebar';
 import { Flex, Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import { Outlet } from 'react-router';
+import classes from './CatalogLayout.module.css';
+import SearchProducts from '@components/SearchProducts/SearchProducts';
 
 const CatalogLayout = () => {
   return (
-    <Layout>
+    <Layout className={classes.main}>
       <Flex gap="middle">
         <CatalogSidebar />
         <Content>
+          <div className={classes.top_bar_container}>
+            <SearchProducts />
+          </div>
           <Flex gap="middle" wrap>
             <Outlet />
           </Flex>
