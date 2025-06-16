1d63a3b92f38fa6a4bcbdfeb45a02ef860adb902
Avechkina <natallia.ovechkina@gmail.com>
2025-06-17 02:20:08 +0300

    feat: clarified technology stack

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -61,6 +61,7 @@ export const STACK = [
     url: 'https://react.dev/',
     img: './images/stack/react.svg',
   },
+
   {
     name: 'TypeScript',
     url: 'https://www.typescriptlang.org/',
@@ -72,14 +73,29 @@ export const STACK = [
     img: './images/stack/vite.svg',
   },
   {
+    name: 'React Router',
+    url: 'https://reactrouter.com/',
+    img: './images/stack/react_router.svg',
+  },
+  {
+    name: 'Zustand',
+    url: 'https://zustand.docs.pmnd.rs',
+    img: './images/stack/zustand.svg',
+  },
+  {
+    name: 'Ant Design',
+    url: 'https://ant.design/',
+    img: './images/stack/ant_design.svg',
+  },
+  {
     name: 'GitHub',
     url: 'https://github.com/',
     img: './images/stack/github.svg',
   },
 ];
 
-export const RSS =   {
-    name: 'RSSchool',
-    url: 'https://rs.school/',
-    img: './images/stack/rss-logo.svg',
-  }
+export const RSS = {
+  name: 'RSSchool',
+  url: 'https://rs.school/',
+  img: './images/stack/rss-logo.svg',
+};
